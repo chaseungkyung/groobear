0f83aea2da256d94895474fe9f19195d9d9f1654
user <user@DESKTOP-FRSDO5M>
2025-02-06 19:31:21 +0900

    02/06 얼렁뚱땅 환경설정

--- groobear/src/main/java/com/sp/app/controller/SettingController.java
+++ groobear/src/main/java/com/sp/app/controller/SettingController.java
@@ -1,20 +1,81 @@
 package com.sp.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.sp.app.model.Member;
+import com.sp.app.model.SessionInfo;
+import com.sp.app.service.MemberService;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/mypage/*")
+@Slf4j
+@RequiredArgsConstructor
 public class SettingController {
-
+	
+	private final MemberService service;
+	
 	@GetMapping("mypage")
 	public String mypage() {
 		return "/mypage/mypage";
 	}
 	
-	@GetMapping("setting")
+	@GetMapping("setting") // not yet setting page
 	public String setting() {
 		return "/mypage/setting";
-	} 
+	}
+	
+	// 비밀번호 확인
+	@PostMapping("pwd")
+	public String checkPwd(
+			@RequestParam(name = "empCode") String empCode,
+			@RequestParam(name = "empPwd") String empPwd, 
+			Model model, HttpSession session) {
+		
+		// 1 패스워드 입력과 확인
+		Member dto = service.loginMember(empCode);
+		
+		//로그인된 사번의 패스워드와 입력 패스워드를 비교하는 문장이 필요할듯
+		if(dto == null || ! empCode.equals(dto.getEmpCode())) {
+			model.addAttribute("message", "비밀번호를 다시 입력해주세요.");
+			
+			return "/mypage/setting";
+		}
+		
+		SessionInfo info = SessionInfo.builder()
+				.empCode(dto.getEmpCode())
+				.empPwd(dto.getEmpPwd())
+				.empName(dto.getEmpName())
+				.empRank(dto.getEmpRank())
+				.teamIdx(dto.getTeamIdx())
+				.build();
+		
+		session.setMaxInactiveInterval(60 * 60);
+		
+		session.setAttribute("member", info);
+		
+		// 설정폼이 뜨는 환경설정 페이지에 넘어감
+		return "/mypage/realSetting";
+	}
+	
+	public String settingForm() {
+		
+		return "redirect:/mypage/";
+	}
+	
+
+	public String settingSubmit() {
+		
+		return "redirect:/mypage/";
+	}
+
+
 }
--- groobear/src/main/webapp/WEB-INF/views/mypage/setting.jsp
+++ groobear/src/main/webapp/WEB-INF/views/mypage/setting.jsp
@@ -6,16 +6,67 @@
 <head>
 	<jsp:include page="/WEB-INF/views/layout/headerResources.jsp"/>
 	<link rel="stylesheet" href="${pageContext.request.contextPath}/dist/css/menu/listMenu.css" type="text/css">
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+	
 </head>
 <body>
 	<jsp:include page="/WEB-INF/views/layout/header.jsp"/>
 	<jsp:include page="/WEB-INF/views/layout/mypageHeader.jsp"/>
 	
-	<main>
-		<div class="mainInner">
-			<h3>환경설정</h3>
-		</div>
-	</main>
-
+	<div class="container">
+		<div class="login-content"> 
+			<form name="loginForm" action="" method="post">
+				<h2 class="title">비밀번호 확인</h2>		
+				<div class="input-div pass">
+					<div class="i">
+						<i class="fas fa-lock"></i>
+					</div>
+					<div class="div">
+						<h5>비밀번호</h5>
+						<input type="password" class="input" name="empPwd" autocomplete="off">
+					</div>
+				</div>
+				<input type="button" class="btn" value="확인" onclick="sendLogin();">
+			</form>
+			<p class="message">${message}</p>
+		</div>		
+	</div>
+	
+	<script type="text/javascript">
+		function sendLogin() {
+			const f = document.loginForm;
+			let str;
+			
+			str = f.empPwd.value.trim();
+			if(! str) {
+				f.empPwd.focus();
+				return;
+			}
+			
+			f.action = '${pageContext.request.contextPath}/pwd';
+			f.submit();
+		}
+		
+		const inputs = document.querySelectorAll(".input");
+		
+		function addcl() {
+			let parent = this.parentNode.parentNode;
+			parent.classList.add("focus");
+		}
+		
+		function remcl(){
+			  let parent = this.parentNode.parentNode;
+			  if(this.value == ""){
+			    parent.classList.remove("focus");
+			 }
+		}
+		
+		inputs.forEach(input => {
+			 input.addEventListener("focus", addcl);
+			 input.addEventListener("blur", remcl);
+		});
+	</script>
 </body>
 </html>
