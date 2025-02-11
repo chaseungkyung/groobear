5e907abcee1626de8651bda04546144ecbe6fc92
E <E@DESKTOP-1T61758>
2025-02-11 15:42:12 +0900

    SettingController update

--- groobear/src/main/java/com/sp/app/controller/SettingController.java
+++ groobear/src/main/java/com/sp/app/controller/SettingController.java
@@ -21,55 +21,12 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class SettingController {
 	
-	private final MemberService service;
-	
-	@GetMapping("setting") // not yet setting page
+	@GetMapping("setting") 
 	public String setting() {
 		return "/mypage/setting";
 	}
 	
-	// 비밀번호 확인
-	@PostMapping("pwd")
-	public String checkPwd(
-			@RequestParam(name = "empCode") String empCode,
-			@RequestParam(name = "empPwd") String empPwd, 
-			Model model, HttpSession session) {
-		
-		// 1 패스워드 입력과 확인
-		Member dto = service.loginMember(empCode);
-		
-		//로그인된 사번의 패스워드와 입력 패스워드를 비교하는 문장이 필요할듯
-		if(dto == null || ! empCode.equals(dto.getEmpCode())) {
-			model.addAttribute("message", "비밀번호를 다시 입력해주세요.");
-			
-			return "/mypage/setting";
-		}
-		
-		SessionInfo info = SessionInfo.builder()
-				.empCode(dto.getEmpCode())
-				.empName(dto.getEmpName())
-				.empRank(dto.getEmpRank())
-				.teamIdx(dto.getTeamIdx())
-				.build();
-		
-		session.setMaxInactiveInterval(60 * 60);
-		
-		session.setAttribute("member", info);
-		
-		// 설정폼이 뜨는 환경설정 페이지에 넘어감
-		return "/mypage/realSetting";
-	}
 	
-	public String settingForm() {
-		
-		return "redirect:/mypage/";
-	}
-	
-
-	public String settingSubmit() {
-		
-		return "redirect:/mypage/";
-	}
 
 
 }
